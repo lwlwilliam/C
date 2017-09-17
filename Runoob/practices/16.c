0192685fa7daed2fa2a34d38b14efa24d3b86f81
William-A <1053317536@qq.com>
2017-09-17 19:30:27 +0800

    runoob practice 13 - 16

---
+++ Runoob/practices/16.c
@@ -0,0 +1,35 @@
+/*
+输入两个正整数m和n，求其最大公约数和最小公倍数。
+程序分析：
+1. 最小公倍数=输入的两个数之积除以它们的最大公约数，关键是求出最大公约数；
+2. 求最大公约数用辗转相除法（又名欧几里德算法）
+	1) 证明：设c是a和b的最大公约数，记为c=gcd(a, b), a >= b
+	令r = a mod b
+	设a = kc, b = jc，则k, j互素，否则c不是最大公约数
+	据上，r = a-mb = kc - mjc = (k - mj)c
+	可知r也是c的倍数，且k - mj与j互素，否则与前述k, j互素矛盾
+	由此可知，b与r的最大公约数也是c，即gcd(a, b) = gcd(b, a mod b)，得证
+	
+*/
+
+#include <stdio.h>
+int main()
+{
+	int a, b, t, r;
+	printf("请输入两个数字：\n");
+	scanf("%d %d", &a, &b);
+	if (a < b) {
+		t = b; // t是两数中的大数
+		b = a;
+		a = t;
+	}
+	r = a % b;
+	int n = a * b;
+	while (r != 0) {
+		a = b;
+		b = r;
+		r = a % b;
+	}
+	printf("这两个数的最大公约数是%d，最小公倍数是%d\n", b, n / b);
+	return 0;
+}
