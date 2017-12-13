630db438724633f22bd3658ee738f2f42c28a283
lwlwilliam <1053317536@qq.com>
2017-12-13 12:31:52 +0800

    c primer plus unit 14

--- CPrimerPlus/Unit14/manybook.c
+++ CPrimerPlus/Unit14/manybook.c
@@ -21,19 +21,24 @@ int main()
 
 	printf("Please enter the book title.\n");
 	printf("Press [enter] at the start of a line to stop.\n");
+
+	// 数量不能超过设定的最大图书数量、获取有效的 title、title 首字符不为空字符
 	while (count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL && library[count].title[0] != '\0') {
 		printf("Now enter the author.\n");
 		s_gets(library[count].author, MAXAUTL);
 		printf("Now enter the value.\n");
-		scanf("%f", &library[count ++].value);
-		while (getchar() != '\n') {
+		scanf("%f", &library[count ++].value); // 获取完 value 之后，数量加 1
+		while (getchar() != '\n') {  // 去除剩余字符
 			continue;
 		}
+
+		// 继续输入图书信息
 		if (count < MAXBKS) {
 			printf("Enter the next title.\n");
 		}
 	}
 
+    // 判断是否有有效的图书信息
 	if (count > 0) {
 		printf("Here is the list of your books:\n");
 		for (index = 0; index < count; index ++) {
