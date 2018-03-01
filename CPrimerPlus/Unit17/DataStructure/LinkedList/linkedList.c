3309a0c4cdd9159b9a8d8d552a4927a91311d86a
lwlwilliam <1053317536@qq.com>
2018-03-01 12:31:51 +0800

    c primer plus: linkedlist

---
+++ CPrimerPlus/Unit17/DataStructure/LinkedList/linkedList.c
@@ -0,0 +1,92 @@
+/* linkedList.c, 支持链表操作的函数 */
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/* 局部函数 */
+static void CopyToNode(Item item, Node *pnode);
+
+/* 接口函数 */
+
+/* 初始化链表 */
+void InitializeList(List *plist)
+{
+    *plist = NULL;
+}
+
+/* 确定链表是否为空 */
+bool ListIsEmpty(const List *plist)
+{
+    if (*plist == NULL) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* 确定链表是否已满 */
+bool ListIsFull(const List *plist)
+{
+    Node *pt;
+    bool full;
+
+    pt = (Node *)malloc(sizeof(Node));
+    if (pt == NULL) {
+        full = true;
+    } else {
+        full = false;
+    }
+    free(pt);
+
+    return full;
+}
+
+/* 确定链表中的项数 */
+unsigned int ListItemCount(const List *plist)
+{
+    unsigned int count = 0;
+    Node *pnode = *plist;  /* 设置链表的开始 */
+
+    while (pnode != NULL) {
+        ++ count;
+        pnode = pnode->next;
+    }
+
+    return count;
+}
+
+/* 在链表的末尾添加项 */
+bool AddItem(Item item, List *plist)
+{
+    Node *pnew;
+    Node *scan = *plist;
+
+    pnew = (Node *) malloc(sizeof(Node));
+    if (pnew == NULL) {
+        return false;  /* 失败时退出函数 */
+    }
+
+    CopyToNode(item, pnew);
+    pnew->next = NULL;
+    if (scan == NULL) {
+        *plist = pnew;
+    } else {
+        while (scan->next != NULL) {
+            scan = scan->next;
+        }
+        scan->next = pnew;
+    }
+
+    return true;
+}
+
+/* 访问每个节点并执行 pfun 指向的函数 */
+void Traverse(const List *plist, void(*pfun)(Item item))
+{
+    Node *pnode = *plist;  /* 设置链表的开始 */
+
+    while (pnode != NULL) {
+        (*pfun)(pnode->item);
+        pnode = pnode->next;
+    }
+}
