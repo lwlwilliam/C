341c120d957952607a052e35c11eca615d79a3a5
lwlwilliam <1053317536@qq.com>
2018-04-24 12:32:11 +0800

    cpp: array and pointer

--- CPP/10 Arrays and Pointers/bounds.c
+++ CPP/10 Arrays and Pointers/bounds.c
@@ -17,11 +17,13 @@ int main()
 	for (i = -1; i < 7; i ++) {
 		printf("%2d %d\n", i, arr[i]);
 	}
+
 	printf("value1 = %d, value2 = %d\n", value1, value2);
+	printf("address of arr[-2]: %p\n", &arr[-2]);
 	printf("address of arr[-1]: %p\n", &arr[-1]);
 	printf("address of arr[4]: %p\n", &arr[4]);
 	printf("address of value1: %p\n", &value1);
 	printf("address of value2: %p\n", &value2);
-
+	
 	return 0;
 }
